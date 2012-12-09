f579d583eedc8656782f14b2afe03650f754af4f
Rohit Choudhary <rohitatmail@yahoo.com>
2012-12-09 04:26:18 -0800

    Create config.php
    This is configuration file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_NAME", "*****"); //Database name
+define("DB_HOST", "******"); //Host name of data base
+define("DB_USER", "******"); //Database user name
+define("DB_PASS", "*****"); //Password of the user for the database
+define("DUMMY_IMAGE_BOY_URL", "http://profile.ak.fbcdn.net/static-ak/rsrc.php/v1/yo/r/UlIqmHJn-SK.gif"); //URL used by facebook for dummy image for male profile
+define("DUMMY_IMAGE_GIRL_URL", "http://profile.ak.fbcdn.net/static-ak/rsrc.php/v1/y9/r/IB7NOFmPw2a.gif"); //URL used by facebook for dummy image for female profile
+define("PROFILE_TABLE_NAME", "profile_info")//Table name of the database where we store the data
+?>
